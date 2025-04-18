ea5535d889d0c1912624dba6abb40e26072c92c0
Niksh-Hiremath <niksh.hiremath24@gmail.com>
2025-04-18 12:00:36 +0530

    port change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "draw",
       script: "/home/niksh_hiremath24/leapdraw-ws/.venv/bin/uvicorn",
-      args: "Backend:app --host 0.0.0.0 --port 443 " +
+      args: "Backend:app --host 0.0.0.0 --port 8080 " +
             "--ssl-keyfile /home/niksh_hiremath24/leapdraw-ws/certs/privkey.pem " +
             "--ssl-certfile /home/niksh_hiremath24/leapdraw-ws/certs/fullchain.pem",
       cwd: "/home/niksh_hiremath24/leapdraw-ws",
