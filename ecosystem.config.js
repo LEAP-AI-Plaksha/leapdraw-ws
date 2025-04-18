8f05d9558ff40188183e06cdf12264c57261e582
Niksh-Hiremath <niksh.hiremath24@gmail.com>
2025-04-18 11:53:57 +0530

    Update SSL certificate paths in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: "draw",
       script: "/home/niksh_hiremath24/leapdraw-ws/.venv/bin/uvicorn",
       args: "Backend:app --host 0.0.0.0 --port 443 " +
-            "--ssl-keyfile /etc/letsencrypt/live/draw.leap-aiclub.tech/privkey.pem " +
-            "--ssl-certfile /etc/letsencrypt/live/draw.leap-aiclub.tech/fullchain.pem",
+            "--ssl-keyfile /home/niksh_hiremath24/leapdraw-ws/certs/privkey.pem " +
+            "--ssl-certfile /home/niksh_hiremath24/leapdraw-ws/certs/fullchain.pem",
       cwd: "/home/niksh_hiremath24/leapdraw-ws",
       interpreter: "none",
       watch: false,
